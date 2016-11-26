c27475bf26593a0941c87cee9f9b5c36c2276ebd
kosdoe <rjcc1@bk.ru>
2016-11-26 15:49:13 +0300

    Create base structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Konstantin on 26.11.2016.
+ */
+var App = function() {
+    console.log('App init');
+};
+
+var app = new App();
+app.get = function() {};
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Konstantin on 26.11.2016.
+ */
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        path: __dirname + "/public",
+        filename: "bundle.js"
+    }
+
+
+
+
+};
